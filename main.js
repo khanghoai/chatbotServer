6f97d3f40e500a45e2e682e35d20c48765caafd6
khanghoai <khanglyhoai1@gmail.com>
2025-08-11 09:52:28 +0700

    upload API

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
+const app = express();
+const port = 3000;
+app.use(express.json());
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const response = openai.responses.create({
-  model: "gpt-4o-mini",
-  input: "3+2",
-  store: true,
-});
+app.post("/getAnswer", (req , res) =>{
+  const {question} = req.body;
+  const response = openai.responses.create({
+    model: "gpt-4o-mini",
+    input: question,
+    store: true,
+  });
+  response.then((result) => res.json(result.output_text));
+})
 
-response.then((result) => console.log(result.output_text));
+app.listen(port, () =>{
+  console.log(`Server đang chạy tại http://localhost:${port}`)
+})
